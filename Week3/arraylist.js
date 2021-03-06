f25c076d31b84b1888fc9da1ad39645e53f8ddb3
nadyajackson <nadya.jackson@bryanuniversity.edu>
2021-03-05 21:30:38 -0800

    Week3 DOM tested

--- Week3/arraylist.js
+++ Week3/arraylist.js
@@ -1,15 +1,22 @@
 var i;
-for (i=0, i<9, i++) {
-var header1 = document.createElement(h1);
-header1.textContent("Hello World");
-document.body.append(header1);
+for (i=0; i<=9; i++) {
+    var header1 = document.createElement('h1');
+    header1.textContent = "Hello World";
+    header1.setAttribute("id", "head");
+    if (i % 2 == 0 ){
+       header1.style.color = "purple";
+    }
+    else{
+        header1.style.color = "green";
+    }
+    document.body.append(header1);
 }
 
-const names = [“steve”, “larry”, “joe”, “shirley”, “steph”, “nate”, “rick”, “emily”];
+const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
 var l;
 
-for (l=0, l <= names.length, l++) {
-var par = document.createElement(p);
-par.textContent(names[l]);
-document.body.append(par);
-}
+for (l=0; l < names.length; l++) {
+    var par = document.createElement('p');
+    par.textContent = names[l];
+    document.body.append(par);
+} 
--- Week3/changestyle.js
+++ Week3/changestyle.js
@@ -1,16 +1,14 @@
 const names = ["Happy", "Sad", "Angry", "Laughter", "Cry"];
 var i;
 
-for (i=0, i <= names.length, i++) {
-  var headers = document.createElement(h2);
+for (i=0; i < names.length; i++) {
+  var headers = document.createElement('h2');
   headers.setAttribute("id", "header")
-  headers.textContent(names[i]);
+  headers.textContent = names[i];
   headers.classList.add("border")
-  document.body.append(headers);
+  headers.style.fontSize = "20px";
+  headers.style.fontWeight = "lighter";
+  headers.style.fontFamily = "sans-serif";
+  headers.style.color = "cornflowerblue";
+document.body.append(headers);
 }
-
-var x = document.getElementById("header");
-x.style.fontSize = "20px";
-x.style.fontWeight = "lighter";
-x.style.fontFamily = "sans-serif";
-x.style.color = "cornflowerblue";
